71d1f1501fee7fad428525b080b77d5fbfa02b6e
Chiva Chen <q932104843@outlook.com>
2024-06-19 22:56:48 +0800

    分布式锁，仓储，redis序列化的修改

--- Mango.Scaffold.Repository/ImpDbContext.cs
+++ Mango.Scaffold.Repository/ImpDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using Mango.EntityFramework;
 using Mango.EntityFramework.Abstractions;
+using Mango.EntityFramework.BaseEntity;
 using Mango.EntityFramework.Enums;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,9 +17,22 @@ namespace Mango.Scaffold.Repository
     /// </summary>
     public class ImpDbContext : BaseDbContext
     {
-        public ImpDbContext(string connnectionString, DbTypeEnum dbTypeEnum, string? migrationsAssemblyName):base(connnectionString, dbTypeEnum, migrationsAssemblyName) { }
-
         public ImpDbContext(DbContextOptions op):base(op) { }
+
+        protected override void OnEntityCreating(Type entityType, ModelBuilder modelBuilder)
+        {
+            if (entityType.BaseType == typeof(TimeFieldEntity))
+            {
+                //设置时间默认值
+                modelBuilder.Entity(entityType.FullName)
+                    .Property(nameof(TimeFieldEntity.CreateTime))
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                modelBuilder.Entity(entityType.FullName)
+                    .Property(nameof(TimeFieldEntity.UpdateTime))
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP");
+            }
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
